b606d10b8ceacd3354c7fc7b87e878b49009bd14
Ian Kane <ianwilliamkane@gmail.com>
2021-12-08 19:25:52 +0000

    Add event listeners for document loading and button clicks

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+    button.addEventListener("click", function() {
+        if (this.getAttribute("data-type") === "submit") {
+            alert("You clicked Sumbit!");
+        } else {
+            let gameType = this.getAttribute("data-type");
+            alert(`You clicked ${gameType}`);
+        }
+
+
+        })
+    }
+})
+
+function runGame() {
+
+
+}
+
+function CheckAnswer () {
+
+
+}
+
+function calculateCorrectAnswer () {
+
+    
+}
+
+function incrementScore () {
+
+    
+}
+
+function incrementWrongAnswer () {
+
+    
+}
+
+function displayAdditionQuestion(){
+
+
+}
+
+function displaySubtractQuestion(){
+
+    
+}
+
+function displayMultiplyQuestion(){
+
+    
+}
